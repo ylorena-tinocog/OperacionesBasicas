43bfb0c4c504cd41d0b9d4e8ae42e19e3affba33
lorena <lorena@lorena-PC>
2016-09-27 21:08:31 -0500

    Adicion clase java

---
+++ src/java/org/ucc/operacionesBasicas/Conexion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ucc.operacionesBasicas;
+
+/**
+ *
+ * @author lorena
+ */
+public class Conexion {
+
+    public Conexion() {
+    }
+    
+    
+    
+}
